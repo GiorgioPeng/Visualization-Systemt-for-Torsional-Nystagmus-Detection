5048b013bd3374a626eae9cd25a9b1aef1072057
giorgiopeng <84867579@qq.com>
2021-06-07 15:43:03 +0800

    .

--- src/subPages/Label/VideoControl.js
+++ src/subPages/Label/VideoControl.js
@@ -3,7 +3,7 @@ import React from 'react'
 function VideoControl() {
     return (
         <div style={{margin:'10px auto',width:'100%',height:'500px',border:'1px solid',display:'flex',justifyContent:'center',alignItems:'center'}}>
-            这个组件用于预览和控制视频播放,还没有具体写，等到后面的演示视频在决定做哪些功能
+            这个组件用于预览和控制视频播放,还没有具体写，等到后面的演示视频在决定做哪些功能，先随便取了个名字
         </div>
     )
 }
